ab6fa02de4f3975d4e77b3dc3665b69d00eb42b8
solbarre <solbarre@example.com>
2018-12-11 00:01:47 +0100

    change 3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola men, World!" );
+        System.out.println( "Hola men, World rrrrrrrr!" );
 
     }
 }
